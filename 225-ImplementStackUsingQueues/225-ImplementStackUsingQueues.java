bfd371040004b34df97b8ecbeed657d47fa1d4df
Keerthiya S <keerthiyakeerthiya360@gmail.com>
2026-07-11 10:38:00 +0530

    Create 225-ImplementStackUsingQueues

---
+++ 225-ImplementStackUsingQueues/225-ImplementStackUsingQueues.java
@@ -0,0 +1,38 @@
+// Last updated: 7/11/2026, 10:37:58 AM
+class MyStack {
+    Queue<Integer> q1 = new LinkedList<>();
+ 
+
+    public MyStack() {
+        
+    }
+    
+      public void push(int x) 
+      {        q1.add(x);        
+      for (int i = 0; i < q1.size() - 1; i++) 
+      {            
+        q1.add(q1.poll());        }  
+          } 
+    
+    public int pop() {
+       return q1.poll();
+        
+    }
+    
+    public int top() {
+       return q1.peek();
+    }
+    
+    public boolean empty() {
+        return q1.isEmpty();
+    }
+}
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack obj = new MyStack();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.top();
+ * boolean param_4 = obj.empty();
+ */
