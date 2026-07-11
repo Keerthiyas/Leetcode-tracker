32eb3856b67f854ea42325ac18a417dab76370c4
Keerthiya S <keerthiyakeerthiya360@gmail.com>
2026-07-11 10:34:51 +0530

    Create 1390-AverageSellingPrice

---
+++ 1390-AverageSellingPrice/1390-AverageSellingPrice.sql
@@ -0,0 +1,8 @@
+-- Last updated: 7/11/2026, 10:34:50 AM
+# Write your MySQL query statement below
+# Write your MySQL query statement below
+SELECT p.product_id, IFNULL(ROUND(SUM(units*price)/SUM(units),2),0) AS average_price
+FROM Prices p LEFT JOIN UnitsSold u
+ON p.product_id = u.product_id AND
+u.purchase_date BETWEEN start_date AND end_date
+group by product_id
