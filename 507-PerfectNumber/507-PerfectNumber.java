164e8c7ef812b2f584322c73be360160da739661
Keerthiya S <keerthiyakeerthiya360@gmail.com>
2026-07-11 10:36:37 +0530

    Create 507-PerfectNumber

---
+++ 507-PerfectNumber/507-PerfectNumber.java
@@ -0,0 +1,10 @@
+// Last updated: 7/11/2026, 10:36:36 AM
+class Solution{
+    public boolean checkPerfectNumber(int num) {
+        if (num == 6 || num == 28 || num == 496 || num == 8128 || num == 33550336) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
