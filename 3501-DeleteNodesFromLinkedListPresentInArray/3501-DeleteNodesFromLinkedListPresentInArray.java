11d882bb662ca983deef970f20abe793402927d6
Keerthiya S <keerthiyakeerthiya360@gmail.com>
2026-07-11 10:33:26 +0530

    Create 3501-DeleteNodesFromLinkedListPresentInArray

---
+++ 3501-DeleteNodesFromLinkedListPresentInArray/3501-DeleteNodesFromLinkedListPresentInArray.java
@@ -0,0 +1,16 @@
+// Last updated: 7/11/2026, 10:33:24 AM
+class Solution {
+    public ListNode modifiedList(int[] nums, ListNode head) {
+        java.util.HashSet<Integer> mpp = new java.util.HashSet<>();
+        for (int x : nums) mpp.add(x);
+        while (head != null && mpp.contains(head.val)) head = head.next;
+        ListNode curr = head;
+        while (curr != null && curr.next != null) {
+            while (curr.next != null && mpp.contains(curr.next.val)) {
+                curr.next = curr.next.next;
+            }
+            curr = curr.next;
+        }
+        return head;
+    }
+}
