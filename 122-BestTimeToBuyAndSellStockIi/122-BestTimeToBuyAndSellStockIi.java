33d57c83d399e820d89ad4e0267fd74c23beb923
Keerthiya S <keerthiyakeerthiya360@gmail.com>
2026-07-13 10:29:33 +0530

    Create 122-BestTimeToBuyAndSellStockIi

---
+++ 122-BestTimeToBuyAndSellStockIi/122-BestTimeToBuyAndSellStockIi.java
@@ -0,0 +1,13 @@
+// Last updated: 7/13/2026, 10:29:30 AM
+class Solution {
+    public int maxProfit(int[] prices) {
+        int max = 0;
+        int start = prices[0];
+        int len = prices.length;
+        for(int i = 1;i<len; i++){
+            if(start < prices[i]) max += prices[i] - start;
+            start = prices[i];
+        }
+        return max;
+    }
+}
