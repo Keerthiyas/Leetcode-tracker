49105a1ecfb67e5094703ce2e31f8bf4fc831541
Keerthiya S <keerthiyakeerthiya360@gmail.com>
2026-07-15 21:33:13 +0530

    File updated at 7/15/2026, 9:33:13 PM

--- 56-MergeIntervals/56-MergeIntervals.java
+++ 56-MergeIntervals/56-MergeIntervals.java
@@ -1,25 +1,16 @@
-// Last updated: 7/15/2026, 9:29:07 PM
+// Last updated: 7/15/2026, 9:33:13 PM
 1class Solution {
-2    public boolean isPalindrome(String s, int left, int right) {
-3        while (left < right) {
-4            if (s.charAt(left) != s.charAt(right)) return false;
-5            left++;
-6            right--;
-7        }
-8        return true;
-9    }
-10
-11    public boolean validPalindrome(String s) {
-12        int left = 0, right = s.length() - 1;
-13
-14        while (left < right) {
-15            if (s.charAt(left) != s.charAt(right)) {
-16                return isPalindrome(s, left + 1, right) || isPalindrome(s, left, right - 1);
-17            }
-18            left++;
-19            right--;
-20        }
-21
-22        return true;
-23    }
-24}
+2    public boolean canConstruct(String ransomNote, String magazine) {
+3		if (ransomNote.length() > magazine.length()) return false;
+4        int[] alphabets_counter = new int[26];
+5        
+6        for (char c : magazine.toCharArray())
+7            alphabets_counter[c-'a']++;
+8
+9        for (char c : ransomNote.toCharArray()){
+10            if (alphabets_counter[c-'a'] == 0) return false;
+11            alphabets_counter[c-'a']--;
+12        }
+13        return true;
+14    }
+15}
