b07c3b70cdc308ae311efca09bbfb847bee7eba5
Keerthiya S <keerthiyakeerthiya360@gmail.com>
2026-07-15 21:36:16 +0530

    File updated at 7/15/2026, 9:36:16 PM

--- 56-MergeIntervals/56-MergeIntervals.java
+++ 56-MergeIntervals/56-MergeIntervals.java
@@ -1,16 +1,17 @@
-// Last updated: 7/15/2026, 9:35:18 PM
+// Last updated: 7/15/2026, 9:36:16 PM
 1class Solution {
-2    public String reverseWords(String s) {
-3        // Split the input string into words and reverse each word
+2    public int countSegments(String s) {
+3        // Split the string into words using spaces as separators
 4        String[] words = s.split(" ");
-5        StringBuilder result = new StringBuilder();
-6        for (String word : words) {
-7            StringBuilder reversedWord = new StringBuilder(word).reverse();
-8            result.append(reversedWord).append(" ");
-9        }
-10        result.deleteCharAt(result.length() - 1);  // Remove the extra space at the end
-11        
-12        // Convert StringBuilder to String
-13        return result.toString();        
-14    }
-15}
+5        int count = 0;
+6
+7        // Count non-empty words
+8        for (String word : words) {
+9            if (!word.isEmpty()) {
+10                count++;
+11            }
+12        }
+13
+14        return count;
+15    }
+16}
