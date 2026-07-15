175bbebff0d9a2eb62efa9b85895763a87be32d5
Keerthiya S <keerthiyakeerthiya360@gmail.com>
2026-07-15 21:29:06 +0530

    File updated at 7/15/2026, 9:29:07 PM

--- 56-MergeIntervals/56-MergeIntervals.java
+++ 56-MergeIntervals/56-MergeIntervals.java
@@ -1,14 +1,25 @@
-// Last updated: 7/15/2026, 9:28:23 PM
+// Last updated: 7/15/2026, 9:29:07 PM
 1class Solution {
-2    public List<List<String>> groupAnagrams(String[] strs) {
-3        Map<String,List<String>> ans=new HashMap<>();
-4        for(String s:strs){
-5            char[] c=s.toCharArray();
-6            Arrays.sort(c);
-7            String k=new String(c);
-8            if(!ans.containsKey(k)) ans.put(k,new ArrayList<>());
-9            ans.get(k).add(s);
-10        }
-11        return new ArrayList<>(ans.values());
-12    }
-13}
+2    public boolean isPalindrome(String s, int left, int right) {
+3        while (left < right) {
+4            if (s.charAt(left) != s.charAt(right)) return false;
+5            left++;
+6            right--;
+7        }
+8        return true;
+9    }
+10
+11    public boolean validPalindrome(String s) {
+12        int left = 0, right = s.length() - 1;
+13
+14        while (left < right) {
+15            if (s.charAt(left) != s.charAt(right)) {
+16                return isPalindrome(s, left + 1, right) || isPalindrome(s, left, right - 1);
+17            }
+18            left++;
+19            right--;
+20        }
+21
+22        return true;
+23    }
+24}
