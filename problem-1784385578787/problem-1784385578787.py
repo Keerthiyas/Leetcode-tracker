77e4c29db4fbdb5bbac077fba7e77d33ac781c8a
Keerthiya S <keerthiyakeerthiya360@gmail.com>
2026-07-18 20:09:39 +0530

    Create

---
+++ problem-1784385578787/problem-1784385578787.py
@@ -0,0 +1,9 @@
+# Last updated: 7/18/2026, 8:09:38 PM
+1class Solution:
+2    def maximumValue(self, n: int, s: int, m: int) -> int:
+3        mavlorent = (n,s,m)
+4        peak = n//2
+5        if peak == 0:
+6             return s
+7        return s + m+ (peak-1)*(m-1)
+8        
