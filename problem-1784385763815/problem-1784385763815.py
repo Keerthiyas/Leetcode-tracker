baa92c84fe0ab682ee56ed51d46521f3a270eace
Keerthiya S <keerthiyakeerthiya360@gmail.com>
2026-07-18 20:12:44 +0530

    Create

---
+++ problem-1784385763815/problem-1784385763815.py
@@ -0,0 +1,13 @@
+# Last updated: 7/18/2026, 8:12:43 PM
+1class Solution:
+2    def rearrangeString(self, s: str, x: str, y: str) -> str:
+3        x_count = s.count(x);
+4        y_count = s.count(y)
+5        ans = []
+6        for ch in s:
+7            if ch!=x  and  ch!=y:
+8                ans.append(ch)
+9        ans.extend(y*y_count)
+10        ans.extend(x * x_count)
+11        return "".join(ans)
+12        
