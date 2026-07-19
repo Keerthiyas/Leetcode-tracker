62fd2609320da6aff7f04272957540b0c6a79f68
Keerthiya S <keerthiyakeerthiya360@gmail.com>
2026-07-19 07:46:23 +0530

    Create 1480-MovieRating

---
+++ 1480-MovieRating/1480-MovieRating.sql
@@ -0,0 +1,17 @@
+-- Last updated: 7/19/2026, 7:46:23 AM
+# Write your MySQL query statement below
+# Write your MySQL query statement below
+(SELECT name AS results
+FROM MovieRating JOIN Users USING(user_id)
+GROUP BY name
+ORDER BY COUNT(*) DESC, name
+LIMIT 1)
+
+UNION ALL
+
+(SELECT title AS results
+FROM MovieRating JOIN Movies USING(movie_id)
+WHERE EXTRACT(YEAR_MONTH FROM created_at) = 202002
+GROUP BY title
+ORDER BY AVG(rating) DESC, title
+LIMIT 1);
