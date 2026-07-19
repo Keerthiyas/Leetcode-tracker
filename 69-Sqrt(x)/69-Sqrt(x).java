e3eebbeb170b1ec8c0d96165f2b49aac7126fab0
Keerthiya S <keerthiyakeerthiya360@gmail.com>
2026-07-19 07:44:29 +0530

    File updated at 7/19/2026, 7:44:28 AM

--- 69-Sqrt(x)/69-Sqrt(x).java
+++ 69-Sqrt(x)/69-Sqrt(x).java
@@ -1,52 +1,24 @@
-// Last updated: 7/19/2026, 7:43:55 AM
+// Last updated: 7/19/2026, 7:44:28 AM
 1class Solution {
-2    public int lower(int[] nums, int n, int target) {
-3        int high = n - 1;
-4        int ans = n;
-5        int low = 0;
-6
-7        while (low <= high) {
-8            int mid = (low + high) / 2;
-9
-10            if (nums[mid] >= target) {
-11                ans = mid;
-12                high = mid - 1;
-13            } else {
-14                low = mid + 1;
-15            }
-16        }
-17
-18        return ans;
-19    }
+2    public boolean searchMatrix(int[][] matrix, int target) {
+3        int rows = matrix.length;
+4        int cols = matrix[0].length;
+5
+6        int i = 0;
+7        int j = cols - 1;
+8
+9        while (i < rows && j >= 0) {
+10            if (matrix[i][j] == target) {
+11                return true;
+12            }
+13
+14            if (matrix[i][j] < target) {
+15                i++;
+16            } else {
+17                j--;
+18            }
+19        }
 20
-21    public int higher(int[] nums, int n, int target) {
-22        int low = 0;
-23        int high = n - 1;
-24        int ans = n;
-25
-26        while (low <= high) {
-27            int mid = (low + high) / 2;
-28
-29            if (nums[mid] > target) {
-30                ans = mid;
-31                high = mid - 1;
-32            } else {
-33                low = mid + 1;
-34            }
-35        }
-36
-37        return ans;
-38    }
-39
-40    public int[] searchRange(int[] nums, int target) {
-41        int n = nums.length;
-42
-43        int lowee = lower(nums, n, target);
-44
-45        if (lowee == n || nums[lowee] != target) {
-46            return new int[] {-1, -1};
-47        }
-48
-49        return new int[] {lowee, higher(nums, n, target) - 1};
-50    }
-51}
+21        return false;
+22    }
+23}
