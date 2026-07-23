48eb71f2d068cb5f53423b361b92230d614bedb9
Keerthiya S <keerthiyakeerthiya360@gmail.com>
2026-07-23 08:57:47 +0530

    File updated at 7/23/2026, 8:57:46 AM

--- 112-PathSum/112-PathSum.java
+++ 112-PathSum/112-PathSum.java
@@ -1,12 +1,34 @@
-// Last updated: 7/23/2026, 8:56:55 AM
-1public class Solution {
-2    public int reverseBits(int n) {
-3        int res = 0;
-4        for (int i = 0; i < 32; i++) {
-5            res <<= 1;
-6            res |= (n & 1);
-7            n >>>= 1;
-8        }
-9        return res;
-10    }
-11}
+// Last updated: 7/23/2026, 8:57:46 AM
+1
+2/**
+3 * Definition for a binary tree node.
+4 * public class TreeNode {
+5 *     int val;
+6 *     TreeNode left;
+7 *     TreeNode right;
+8 *     TreeNode() {}
+9 *     TreeNode(int val) { this.val = val; }
+10 *     TreeNode(int val, TreeNode left, TreeNode right) {
+11 *         this.val = val;
+12 *         this.left = left;
+13 *         this.right = right;
+14 *     }
+15 * }
+16 */
+17class Solution {
+18    public int minDepth(TreeNode root) {
+19        if(root==null)
+20        {
+21            return 0;
+22        }
+23        if(root.left==null)
+24        {
+25            return 1+minDepth(root.right);
+26        }
+27        if(root.right==null)
+28        {
+29            return 1+minDepth(root.left);
+30        }
+31        return 1+Math.min(minDepth(root.left),minDepth(root.right));
+32    }
+33}
