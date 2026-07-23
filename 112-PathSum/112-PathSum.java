875c82f0ed3e8481cb28c5e70dabbeadcfce21a8
Keerthiya S <keerthiyakeerthiya360@gmail.com>
2026-07-23 09:04:49 +0530

    File updated at 7/23/2026, 9:04:49 AM

--- 112-PathSum/112-PathSum.java
+++ 112-PathSum/112-PathSum.java
@@ -1,19 +1,33 @@
-// Last updated: 7/23/2026, 9:03:23 AM
+// Last updated: 7/23/2026, 9:04:49 AM
 1class Solution {
-2    public int minimumTotal(List<List<Integer>> triangle) {
-3        int row = triangle.size();
-4        int[] memo = new int[row];
+2    public List<List<Integer>> threeSum(int[] nums) {
+3        List<List<Integer>> result = new ArrayList<>();
+4        Arrays.sort(nums);
 5
-6        for (int i = 0; i < row; i++) {
-7            memo[i] = triangle.get(row - 1).get(i);
-8        }
-9
-10        for (int r = row-2; r >= 0; r--) {
-11            for (int c = 0; c <= r; c++) {
-12                memo[c] = Math.min(memo[c], memo[c+1]) + triangle.get(r).get(c);
-13            }
-14        }
+6        for (int i = 0; i < nums.length - 2; i++) {
+7            if (i > 0 && nums[i] == nums[i-1]) continue; // skip duplicate i
+8
+9            int left = i + 1;
+10            int right = nums.length - 1;
+11            int sum = nums[i] * -1;
+12
+13            while (left < right) {
+14                int s = nums[left] + nums[right];
 15
-16        return memo[0];        
-17    }
-18}
+16                if (sum == s) {
+17                    result.add(Arrays.asList(nums[i], nums[left], nums[right]));
+18                    left++;
+19                    right--;
+20
+21                    while (left < right && nums[left] == nums[left-1]) left++;
+22                    while (left < right && nums[right] == nums[right+1]) right--;
+23                } else if (s < sum) {
+24                    left++;
+25                } else {
+26                    right--;
+27                }
+28            }
+29        }
+30        return result;
+31    }
+32}
