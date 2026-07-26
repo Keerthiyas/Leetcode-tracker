98fc1509bfb43d419a17c2f7d87266ec62719c6e
Keerthiya S <keerthiyakeerthiya360@gmail.com>
2026-07-26 18:49:12 +0530

    File updated at 7/26/2026, 6:49:12 PM

--- 1528-ShuffleString/1528-ShuffleString.java
+++ 1528-ShuffleString/1528-ShuffleString.java
@@ -1,17 +1,13 @@
-// Last updated: 7/26/2026, 6:47:37 PM
+// Last updated: 7/26/2026, 6:49:12 PM
 1class Solution {
-2    public String restoreString(String s, int[] indices) {
-3        
-4        int length=s.length();
-5        StringBuilder sb=new StringBuilder("");
-6         char c[]=new char[length];
-7       
-8        for(int i=0;i<length;i++){
-9
-10            c[indices[i]]=s.charAt(i);
-11
-12        }
-13        sb.append(c);
-14        return sb.toString();
-15    }
-16}
+2    public List<Integer> findWordsContaining(String[] words, char x) {
+3        List<Integer> list=new ArrayList<>(words.length);
+4        for(int j=0;j<words.length;j++){
+5            if(words[j].indexOf(x)!=-1){
+6                list.add(j);
+7            }
+8        }    
+9        return list;
+10        
+11    }
+12}
