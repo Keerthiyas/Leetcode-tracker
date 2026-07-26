6b53d681ddbfc7cf199ecc9503dc8d73851424da
Keerthiya S <keerthiyakeerthiya360@gmail.com>
2026-07-26 18:56:19 +0530

    File updated at 7/26/2026, 6:56:19 PM

--- 1528-ShuffleString/1528-ShuffleString.java
+++ 1528-ShuffleString/1528-ShuffleString.java
@@ -1,4 +1,4 @@
-// Last updated: 7/26/2026, 6:54:24 PM
+// Last updated: 7/26/2026, 6:56:19 PM
 1/**
 2 * Definition for singly-linked list.
 3 * public class ListNode {
@@ -10,15 +10,58 @@
 9 * }
 10 */
 11class Solution {
-12    public ListNode removeNthFromEnd(ListNode head, int n) {
-13        ListNode fast = head, slow = head;
-14        for (int i = 0; i < n; i++) fast = fast.next;
-15        if (fast == null) return head.next;
-16        while (fast.next != null) {
-17            fast = fast.next;
-18            slow = slow.next;
-19        }
-20        slow.next = slow.next.next;
-21        return head;
-22    }
-23}
+12
+13    public void reorderList(ListNode head) {
+14
+15        if(head == null || head.next == null){
+16            return;
+17        }
+18
+19        // Step 1: Find Middle
+20
+21        ListNode slow = head;
+22        ListNode fast = head;
+23
+24        while(fast.next != null && fast.next.next != null){
+25            slow = slow.next;
+26            fast = fast.next.next;
+27        }
+28
+29        // Step 2: Split List
+30
+31        ListNode second = slow.next;
+32        slow.next = null;
+33
+34        // Step 3: Reverse Second Half
+35
+36        ListNode prev = null;
+37
+38        while(second != null){
+39
+40            ListNode next = second.next;
+41
+42            second.next = prev;
+43
+44            prev = second;
+45            second = next;
+46        }
+47
+48        second = prev;
+49
+50        // Step 4: Merge Both Halves
+51
+52        ListNode first = head;
+53
+54        while(second != null){
+55
+56            ListNode temp1 = first.next;
+57            ListNode temp2 = second.next;
+58
+59            first.next = second;
+60            second.next = temp1;
+61
+62            first = temp1;
+63            second = temp2;
+64        }
+65    }
+66}
