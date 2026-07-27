205fc5c14d7662edd923193a1ebf2b972bd0b628
Keerthiya S <keerthiyakeerthiya360@gmail.com>
2026-07-27 09:39:44 +0530

    File updated at 7/27/2026, 9:39:44 AM

--- 132-PalindromePartitioningII/132-PalindromePartitioningII.java
+++ 132-PalindromePartitioningII/132-PalindromePartitioningII.java
@@ -1,33 +1,22 @@
-// Last updated: 7/27/2026, 9:09:27 AM
+// Last updated: 7/27/2026, 9:39:44 AM
 1class Solution {
-2    public int minCut(String s) {
-3        int n = s.length();
-4        int[] dp = new int[n];
-5        Arrays.fill(dp, -1);
-6        return solve(0, s, dp) - 1; // cuts = partitions - 1;
-7    }
-8    private int solve(int i, String s, int[] dp){
-9        int n = s.length();
-10        if(i == n) return 0;
-11
-12        if(dp[i] != -1) return dp[i];
-13
-14        int minCost = Integer.MAX_VALUE;
-15
-16        for(int j = i; j < n; j++){
-17            if(isPalindrome(s, i, j)){
-18                int cost = 1 + solve(j + 1, s, dp);
-19                minCost = Math.min(minCost, cost);
-20            }
-21        }
-22        return dp[i] = minCost;
-23    }
-24    private boolean isPalindrome(String s, int i, int j){
-25        while(i < j){
-26            if(s.charAt(i) != s.charAt(j)) return false;
-27            i++;
-28            j--;
-29        }
-30        return true;
-31    }
-32}
+2    public int findMin(int[] nums) {
+3        int n = nums.length - 1;
+4        int last = nums[n];
+5        int left = 0, right = n;
+6
+7        while (left < n && nums[left] == last)
+8            left++;
+9
+10        while (left < right) {
+11            int mid = left + right >> 1;
+12
+13            if (nums[mid] > last)
+14                left = mid + 1;
+15            else
+16                right = mid;
+17        }
+18
+19        return nums[left];
+20    }
+21}
