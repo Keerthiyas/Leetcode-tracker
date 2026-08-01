08d1a58b9ae20ebef4a1244faa311ed288625d00
Keerthiya S <keerthiyakeerthiya360@gmail.com>
2026-08-01 12:48:06 +0530

    File updated at 8/1/2026, 12:48:05 PM

--- 303-RangeSumQuery-Immutable/303-RangeSumQuery-Immutable.java
+++ 303-RangeSumQuery-Immutable/303-RangeSumQuery-Immutable.java
@@ -1,17 +1,33 @@
-// Last updated: 8/1/2026, 12:46:40 PM
+// Last updated: 8/1/2026, 12:48:05 PM
 1class Solution {
-2    public int[] countBits(int n) {
-3        int[] dp = new int[n + 1];
-4        int sub = 1;
-5
-6        for (int i = 1; i <= n; i++) {
-7            if (sub * 2 == i) {
-8                sub = i;
-9            }
-10
-11            dp[i] = dp[i - sub] + 1;
-12        }
-13
-14        return dp;        
-15    }
-16}
+2    public String reverseVowels(String s) {
+3        char[] word = s.toCharArray();
+4        int start = 0;
+5        int end = s.length() - 1;
+6        String vowels = "aeiouAEIOU";
+7        
+8        while (start < end) {
+9            // Move start pointer until it points to a vowel
+10            while (start < end && vowels.indexOf(word[start]) == -1) {
+11                start++;
+12            }
+13            
+14            // Move end pointer until it points to a vowel
+15            while (start < end && vowels.indexOf(word[end]) == -1) {
+16                end--;
+17            }
+18            
+19            // Swap the vowels
+20            char temp = word[start];
+21            word[start] = word[end];
+22            word[end] = temp;
+23            
+24            // Move the pointers towards each other
+25            start++;
+26            end--;
+27        }
+28        
+29        String answer = new String(word);
+30        return answer;
+31    }
+32}
