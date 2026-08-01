aa9cc4694b0a5cb0a5c0d86a45f1aac5ac0f0199
Keerthiya S <keerthiyakeerthiya360@gmail.com>
2026-08-01 13:01:44 +0530

    File updated at 8/1/2026, 1:01:44 PM

--- 303-RangeSumQuery-Immutable/303-RangeSumQuery-Immutable.java
+++ 303-RangeSumQuery-Immutable/303-RangeSumQuery-Immutable.java
@@ -1,33 +1,31 @@
-// Last updated: 8/1/2026, 12:48:05 PM
+// Last updated: 8/1/2026, 1:01:44 PM
 1class Solution {
-2    public String reverseVowels(String s) {
-3        char[] word = s.toCharArray();
-4        int start = 0;
-5        int end = s.length() - 1;
-6        String vowels = "aeiouAEIOU";
-7        
-8        while (start < end) {
-9            // Move start pointer until it points to a vowel
-10            while (start < end && vowels.indexOf(word[start]) == -1) {
-11                start++;
-12            }
-13            
-14            // Move end pointer until it points to a vowel
-15            while (start < end && vowels.indexOf(word[end]) == -1) {
-16                end--;
-17            }
-18            
-19            // Swap the vowels
-20            char temp = word[start];
-21            word[start] = word[end];
-22            word[end] = temp;
-23            
-24            // Move the pointers towards each other
-25            start++;
-26            end--;
-27        }
-28        
-29        String answer = new String(word);
-30        return answer;
-31    }
-32}
+2    public int sumOfLeftLeaves(TreeNode root) {
+3        if (root == null) {
+4            return 0;
+5        }
+6        
+7        Queue<Pair<TreeNode, Boolean>> queue = new LinkedList<>();
+8        queue.offer(new Pair<>(root, false));  // (node, is_left)
+9        int totalSum = 0;
+10        
+11        while (!queue.isEmpty()) {
+12            Pair<TreeNode, Boolean> pair = queue.poll();
+13            TreeNode node = pair.getKey();
+14            boolean isLeft = pair.getValue();
+15            
+16            if (isLeft && node.left == null && node.right == null) {
+17                totalSum += node.val;
+18            }
+19            
+20            if (node.left != null) {
+21                queue.offer(new Pair<>(node.left, true));
+22            }
+23            if (node.right != null) {
+24                queue.offer(new Pair<>(node.right, false));
+25            }
+26        }
+27        
+28        return totalSum;
+29    }
+30}
