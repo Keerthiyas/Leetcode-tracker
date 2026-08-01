18109ac3cc4ed5520ffb11ee40299074d6ebc7dd
Keerthiya S <keerthiyakeerthiya360@gmail.com>
2026-08-01 13:16:06 +0530

    File updated at 8/1/2026, 1:16:06 PM

--- 303-RangeSumQuery-Immutable/303-RangeSumQuery-Immutable.java
+++ 303-RangeSumQuery-Immutable/303-RangeSumQuery-Immutable.java
@@ -1,15 +1,20 @@
-// Last updated: 8/1/2026, 1:10:25 PM
-1
-2class Solution {
-3    public int arrangeCoins(int n) {
-4        long left = 1, right = n;
-5        while (left <= right) {
-6            long mid = left + (right - left) / 2;
-7            long coins_needed = mid * (mid + 1) / 2;
-8            if (coins_needed == n) return (int) mid;
-9            else if (coins_needed < n) left = mid + 1;
-10            else right = mid - 1;
-11        }
-12        return (int) right;
-13    }
-14}
+// Last updated: 8/1/2026, 1:16:06 PM
+1class Solution {
+2    public int findContentChildren(int[] g, int[] s) {
+3		int i = 0;
+4		int j = 0;
+5		int count = 0;
+6		Arrays.sort(g);
+7		Arrays.sort(s);
+8		while (i < g.length && j < s.length) {
+9			if (s[j] >= g[i]) {
+10				i++;
+11				j++;
+12				count++;
+13			}else {
+14				j++;
+15			}
+16		}
+17		return count;
+18    }
+19}
