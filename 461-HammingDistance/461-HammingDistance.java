417748efc835eeb4a80ae8c9a6005f65e2841826
Keerthiya S <keerthiyakeerthiya360@gmail.com>
2026-08-02 13:01:43 +0530

    File updated at 8/2/2026, 1:01:43 PM

--- 461-HammingDistance/461-HammingDistance.java
+++ 461-HammingDistance/461-HammingDistance.java
@@ -1,26 +1,12 @@
-// Last updated: 8/2/2026, 1:00:57 PM
-1public class Solution {
-2    public int islandPerimeter(int[][] grid) {
-3        int n = grid.length;
-4        int m = grid[0].length;
-5        int cnt = 0;
-6        for (int i = 0; i < n; i++) {
-7            for (int j = 0; j < m; j++) {
-8                if (grid[i][j] == 1) {
-9                    if ((j > 0 && grid[i][j - 1] == 0) || j == 0)
-10                        cnt++;
-11
-12                    if ((i > 0 && grid[i - 1][j] == 0) || i == 0)
-13                        cnt++;
-14
-15                    if ((j < m - 1 && grid[i][j + 1] == 0) || j == m - 1)
-16                        cnt++;
-17
-18                    if ((i < n - 1 && grid[i + 1][j] == 0) || i == n - 1)
-19                        cnt++;
-20                }
-21            }
-22        }
-23        return cnt;
-24    }
-25}
+// Last updated: 8/2/2026, 1:01:43 PM
+1class Solution {
+2    public int findComplement(int num) {
+3        if (num == 0) return 1;
+4
+5        int bitLength = Integer.toBinaryString(num).length();
+6        
+7        int mask = (1 << bitLength) - 1;
+8        
+9        return num ^ mask;
+10    }
+11}
