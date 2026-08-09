bf91b67f418a518aab80daa5b64072a271e64a77
Keerthiya S <keerthiyakeerthiya360@gmail.com>
2026-08-09 20:57:33 +0530

    File updated at 8/9/2026, 8:57:34 PM

--- 492-ConstructtheRectangle/492-ConstructtheRectangle.java
+++ 492-ConstructtheRectangle/492-ConstructtheRectangle.java
@@ -1,10 +1,19 @@
-// Last updated: 8/9/2026, 7:59:19 PM
+// Last updated: 8/9/2026, 8:57:34 PM
 1class Solution {
-2       public String licenseKeyFormatting(String s, int k) {
-3        StringBuilder sb = new StringBuilder();
-4        for (int i = s.length() - 1; i >= 0; i--)
-5            if (s.charAt(i) != '-')
-6                sb.append(sb.length() % (k + 1) == k ? '-' : "").append(s.charAt(i));
-7        return sb.reverse().toString().toUpperCase();
-8    } 
-9}
+2    public int maxSubarrayLength(int[] nums, int k) {
+3        int ans = 0;
+4        HashMap<Integer, Integer> mp = new HashMap<>();
+5        int l = 0;
+6
+7        for (int r = 0; r < nums.length; r++) {
+8            mp.put(nums[r], mp.getOrDefault(nums[r], 0) + 1);
+9            while (mp.get(nums[r]) > k) {
+10                mp.put(nums[l], mp.get(nums[l]) - 1);
+11                l++;
+12            }
+13            ans = Math.max(ans, r - l + 1);
+14        }
+15
+16        return ans;
+17    }
+18}
