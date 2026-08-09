8927ae0882bc503f78c98f7b69aff75fe5e51767
Keerthiya S <keerthiyakeerthiya360@gmail.com>
2026-08-09 21:13:16 +0530

    File updated at 8/9/2026, 9:13:16 PM

--- 492-ConstructtheRectangle/492-ConstructtheRectangle.cpp
+++ 492-ConstructtheRectangle/492-ConstructtheRectangle.cpp
@@ -1,19 +1,20 @@
-// Last updated: 8/9/2026, 9:00:49 PM
-1
-2class Solution {
-3public:
-4    int equalSubstring(string s, string t, int maxCost) {
-5        int left=0;
-6        int sum=0, ans=0;
-7
-8        for(int right=0; right<s.length(); right++){
-9            sum += abs(s[right]-t[right]);
-10            while(sum>maxCost){
-11                sum -= abs(s[left]-t[left]);
-12                left++;
+// Last updated: 8/9/2026, 9:13:16 PM
+1class Solution {
+2public:
+3    int peakIndexInMountainArray(vector<int>& nums) {
+4        int n=nums.size();
+5        if(n==1) return 0;
+6
+7        int low=0,high=n-1;
+8        while(low<=high){
+9            int mid=low+(high-low)/2;
+10            if(mid-1>=0 && mid+1<n &&
+11                nums[mid]>nums[mid-1] && nums[mid]>nums[mid+1]){
+12                return mid;
 13            }
-14            ans=max(ans,right-left+1);
-15        }
-16        return ans;
-17    }
-18};
+14            if( nums[mid]<nums[mid+1]) low=mid+1;
+15            else high=mid-1;
+16        }
+17        return -1;
+18    }
+19};
