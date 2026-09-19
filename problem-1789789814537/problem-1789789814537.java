48aeaab1bd27ee1a8214837f048d5ccd3c16dbec
Keerthiya S <keerthiyakeerthiya360@gmail.com>
2026-09-19 09:20:15 +0530

    Create

---
+++ problem-1789789814537/problem-1789789814537.java
@@ -0,0 +1,8 @@
+// Last updated: 9/19/2026, 9:20:14 AM
+1class Solution {
+2    public int strStr(String haystack, String needle) {
+3
+4        return haystack.indexOf(needle);
+5        
+6    }
+7}
