bc050ea2321de7fbddce1c14c5fb356f825e2b00
Keerthiya S <keerthiyakeerthiya360@gmail.com>
2026-09-24 20:49:47 +0530

    File updated at 9/24/2026, 8:49:47 PM

--- 104-MaximumDepthofBinaryTree/104-MaximumDepthofBinaryTree.java
+++ 104-MaximumDepthofBinaryTree/104-MaximumDepthofBinaryTree.java
@@ -1,36 +1,19 @@
-// Last updated: 9/24/2026, 8:48:53 PM
+// Last updated: 9/24/2026, 8:49:47 PM
 1class Solution {
-2    public void flatten(TreeNode root) {
-3        TreeNode curr = root;
-4
-5        while (curr != null) {
-6
-7            // If there is no left subtree,
-8            // the node is already in the correct position.
-9            if (curr.left == null) {
-10                curr = curr.right;
-11                continue;
-12            }
-13
-14            // Find the rightmost node of the left subtree
-15            TreeNode predecessor = curr.left;
-16
-17            while (predecessor.right != null) {
-18                predecessor = predecessor.right;
-19            }
-20
-21            // Connect the rightmost node of the left subtree
-22            // to the original right subtree.
-23            predecessor.right = curr.right;
-24
-25            // Move the left subtree to the right
-26            curr.right = curr.left;
-27
-28            // Left pointer must always be null
-29            curr.left = null;
-30
-31            // Move to the next node
-32            curr = curr.right;
-33        }
-34    }
-35}
+2    public int rob(TreeNode root) {
+3        int[] option=helper(root);
+4        return Math.max(option[0],option[1]);
+5    }
+6    public int[] helper(TreeNode root){
+7        if(root==null) return new int[2];
+8
+9        int[] left=helper(root.left);
+10        int[] right=helper(root.right);
+11        int[] option=new int[2];
+12
+13        option[0]=root.val+ left[1]+right[1];
+14        option[1]=Math.max(left[0],left[1])+Math.max(right[0],right[1]);
+15
+16        return option;
+17    }
+18}
