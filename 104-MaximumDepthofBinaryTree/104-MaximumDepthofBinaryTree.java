67aa0650923c44e8dfb0c4dbcd825853ba884294
Keerthiya S <keerthiyakeerthiya360@gmail.com>
2026-09-24 21:10:56 +0530

    File updated at 9/24/2026, 9:10:55 PM

--- 104-MaximumDepthofBinaryTree/104-MaximumDepthofBinaryTree.java
+++ 104-MaximumDepthofBinaryTree/104-MaximumDepthofBinaryTree.java
@@ -1,55 +1,44 @@
-// Last updated: 9/24/2026, 9:10:01 PM
-1/**
-2 * Definition for binary tree
-3 * public class TreeNode {
-4 *     int val;
-5 *     TreeNode left;
-6 *     TreeNode right;
-7 *     TreeNode(int x) { val = x; }
-8 * }
-9 */
-10
-11public class BSTIterator {
-12    
-13    private Stack<TreeNode> stack;
-14    public BSTIterator(TreeNode root) {
-15        stack = new Stack<>();
-16        TreeNode cur = root;
-17        while(cur != null){
-18            stack.push(cur);
-19            if(cur.left != null)
-20                cur = cur.left;
-21            else
-22                break;
-23        }
-24    }
-25
-26    /** @return whether we have a next smallest number */
-27    public boolean hasNext() {
-28        return !stack.isEmpty();
-29    }
-30
-31    /** @return the next smallest number */
-32    public int next() {
-33        TreeNode node = stack.pop();
-34        TreeNode cur = node;
-35        // traversal right branch
-36        if(cur.right != null){
-37            cur = cur.right;
-38            while(cur != null){
-39                stack.push(cur);
-40                if(cur.left != null)
-41                    cur = cur.left;
-42                else
-43                    break;
-44            }
-45        }
-46        return node.val;
-47    }
-48}
-49
-50/**
-51 * Your BSTIterator will be called like this:
-52 * BSTIterator i = new BSTIterator(root);
-53 * while (i.hasNext()) v[f()] = i.next();
-54 */
+// Last updated: 9/24/2026, 9:10:55 PM
+1
+2class Solution {
+3    public TreeNode deleteNode(TreeNode root, int key) {
+4        //search
+5        if(root == null ) {
+6            return null;
+7        }
+8        if(root.val<key){
+9            root.right = deleteNode(root.right,key);
+10        }
+11        else if(root.val>key) {
+12            root.left = deleteNode(root.left,key);
+13        }
+14        else{
+15            //case 1: leaf node
+16            if(root.left == null && root.right == null ) {
+17                return null;
+18            }
+19            //case 2 : one child
+20            if(root.left ==  null){
+21                return root.right;
+22            }
+23            else if(root.right ==  null ) {
+24                return root.left;
+25            }
+26            //case 3 : two children
+27            TreeNode IS = findInOrderSuccessor(root.right);
+28            root.val = IS.val;
+29
+30            
+31            root.right = deleteNode(root.right,IS.val);
+32        }
+33        return root;
+34
+35    }
+36    private TreeNode findInOrderSuccessor(TreeNode root){
+37        while(root.left != null){
+38            root = root.left;
+39        }
+40        return root;
+41    }
+42}
+43
