8a3840bee506fa8c1c5acd1be0ab20b5255c6ce6
Keerthiya S <keerthiyakeerthiya360@gmail.com>
2026-09-24 21:05:53 +0530

    File updated at 9/24/2026, 9:05:53 PM

--- 104-MaximumDepthofBinaryTree/104-MaximumDepthofBinaryTree.java
+++ 104-MaximumDepthofBinaryTree/104-MaximumDepthofBinaryTree.java
@@ -1,33 +1,16 @@
-// Last updated: 9/24/2026, 9:02:57 PM
+// Last updated: 9/24/2026, 9:05:53 PM
 1class Solution {
-2    public TreeNode buildTree(int[] preorder, int[] inorder) {
-3        Deque<Integer> preorderQueue = new ArrayDeque<>();
-4        for (int val : preorder) {
-5            preorderQueue.offer(val);
-6        }
-7
-8        return build(preorderQueue, inorder);        
-9    }
-10
-11    private TreeNode build(Deque<Integer> preorder, int[] inorder) {
-12        if (inorder.length > 0) {
-13            int idx = indexOf(inorder, preorder.poll());
-14            TreeNode root = new TreeNode(inorder[idx]);
-15
-16            root.left = build(preorder, Arrays.copyOfRange(inorder, 0, idx));
-17            root.right = build(preorder, Arrays.copyOfRange(inorder, idx + 1, inorder.length));
-18
-19            return root;
-20        }
-21        return null;
-22    }
-23
-24    private int indexOf(int[] arr, int value) {
-25        for (int i = 0; i < arr.length; i++) {
-26            if (arr[i] == value) {
-27                return i;
-28            }
-29        }
-30        return -1; // shouldn't happen with valid input
-31    }    
-32}
+2    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+3        int small = Math.min(p.val, q.val);
+4        int large = Math.max(p.val, q.val);
+5        while (root != null) {
+6            if (root.val > large) // p, q belong to the left subtree
+7                root = root.left;
+8            else if (root.val < small) // p, q belong to the right subtree
+9                root = root.right;
+10            else // Now, small <= root.val <= large -> This root is the LCA between p and q
+11                return root;
+12        }
+13        return null;
+14    }
+15}
