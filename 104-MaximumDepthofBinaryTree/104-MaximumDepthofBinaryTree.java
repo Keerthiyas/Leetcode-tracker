8ecb8ea61d322de98174c16736073a5a6ad5f404
Keerthiya S <keerthiyakeerthiya360@gmail.com>
2026-09-24 19:50:30 +0530

    File updated at 9/24/2026, 7:50:30 PM

--- 104-MaximumDepthofBinaryTree/104-MaximumDepthofBinaryTree.java
+++ 104-MaximumDepthofBinaryTree/104-MaximumDepthofBinaryTree.java
@@ -1,36 +1,21 @@
-// Last updated: 9/24/2026, 7:49:45 PM
-1class Solution {
-2    public List<List<Integer>> levelOrderBottom(TreeNode root) {
-3        List<List<Integer>> result = new ArrayList<>();
-4
-5        if (root == null) {
-6            return result;
-7        }
-8
-9        Queue<TreeNode> queue = new LinkedList<>();
-10        queue.add(root);
-11
-12        while (!queue.isEmpty()) {
-13            int levelSize = queue.size();
-14            List<Integer> currentLevel = new ArrayList<>(levelSize);
-15
-16            for (int i = 0; i < levelSize; i++) {
-17                TreeNode currentNode = queue.poll();
-18
-19                currentLevel.add(currentNode.val);
-20
-21                if (currentNode.left != null) {
-22                    queue.add(currentNode.left);
-23                }
-24
-25                if (currentNode.right != null) {
-26                    queue.add(currentNode.right);
-27                }
-28            }
-29
-30            result.add(0, currentLevel);
-31        }
-32
-33        return result;
-34    }
-35}
+// Last updated: 9/24/2026, 7:50:30 PM
+1public class Solution {
+2    public List<Integer> rightSideView(TreeNode root) {
+3        List<Integer> result = new ArrayList<Integer>();
+4        rightView(root, result, 0);
+5        return result;
+6    }
+7    
+8    public void rightView(TreeNode curr, List<Integer> result, int currDepth){
+9        if(curr == null){
+10            return;
+11        }
+12        if(currDepth == result.size()){
+13            result.add(curr.val);
+14        }
+15        
+16        rightView(curr.right, result, currDepth + 1);
+17        rightView(curr.left, result, currDepth + 1);
+18        
+19    }
+20}
