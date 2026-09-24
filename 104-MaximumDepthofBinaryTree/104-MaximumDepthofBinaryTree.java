625584b8994159133a5051fee302196ad11c296a
Keerthiya S <keerthiyakeerthiya360@gmail.com>
2026-09-24 20:58:01 +0530

    File updated at 9/24/2026, 8:57:59 PM

--- 104-MaximumDepthofBinaryTree/104-MaximumDepthofBinaryTree.java
+++ 104-MaximumDepthofBinaryTree/104-MaximumDepthofBinaryTree.java
@@ -1,58 +1,57 @@
-// Last updated: 9/24/2026, 8:56:23 PM
-1/*
-2// Definition for a Node.
-3class Node {
-4    public int val;
-5    public Node left;
-6    public Node right;
-7    public Node next;
-8
-9    public Node() {}
-10
-11    public Node(int _val) {
-12        val = _val;
-13    }
-14
-15    public Node(int _val, Node _left, Node _right, Node _next) {
-16        val = _val;
-17        left = _left;
-18        right = _right;
-19        next = _next;
-20    }
-21};
-22*/
-23
-24class Solution {
-25    public Node connect(Node root) {
-26        Node curr = root;
-27
-28        while (curr != null) {
-29            // Dummy node helps build the next level
-30            Node dummy = new Node(0);
-31            Node tail = dummy;
+// Last updated: 9/24/2026, 8:57:59 PM
+1class Solution {
+2    public List<Integer> distanceK(TreeNode root, TreeNode target, int k) {
+3        List<Integer> ans = new ArrayList<>();
+4        Map<Integer, TreeNode> parent = new HashMap<>();
+5        Queue<TreeNode> queue = new LinkedList<>();
+6        queue.offer(root);
+7
+8        while (!queue.isEmpty()) {
+9            int size = queue.size();
+10            for (int i = 0; i < size; i++) {
+11                TreeNode top = queue.poll();
+12
+13                if (top.left != null) {
+14                    parent.put(top.left.val, top);
+15                    queue.offer(top.left);
+16                }
+17
+18                if (top.right != null) {
+19                    parent.put(top.right.val, top);
+20                    queue.offer(top.right);
+21                }
+22            }
+23        }
+24
+25        Map<Integer, Integer> visited = new HashMap<>();
+26        queue.offer(target);
+27        while (k > 0 && !queue.isEmpty()) {
+28            int size = queue.size();
+29
+30            for (int i = 0; i < size; i++) {
+31                TreeNode top = queue.poll();
 32
-33            // Traverse current level using next pointers
-34            while (curr != null) {
-35
-36                // Add left child to next level
-37                if (curr.left != null) {
-38                    tail.next = curr.left;
-39                    tail = tail.next;
-40                }
-41
-42                // Add right child to next level
-43                if (curr.right != null) {
-44                    tail.next = curr.right;
-45                    tail = tail.next;
-46                }
+33                visited.put(top.val, 1);
+34
+35                if (top.left != null && !visited.containsKey(top.left.val)) {
+36                    queue.offer(top.left);
+37                }
+38
+39                if (top.right != null && !visited.containsKey(top.right.val)) {
+40                    queue.offer(top.right);
+41                }
+42
+43                if (parent.containsKey(top.val) && !visited.containsKey(parent.get(top.val).val)) {
+44                    queue.offer(parent.get(top.val));
+45                }
+46            }
 47
-48                curr = curr.next;
-49            }
+48            k--;
+49        }
 50
-51            // Move to the first node of the next level
-52            curr = dummy.next;
+51        while (!queue.isEmpty()) {
+52            ans.add(queue.poll().val);
 53        }
-54
-55        return root;
-56    }
-57}
+54        return ans;
+55    }
+56}
