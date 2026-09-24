4882d00ab3877bf815a5563f83ad08b097dbdfcf
Keerthiya S <keerthiyakeerthiya360@gmail.com>
2026-09-24 20:45:24 +0530

    File updated at 9/24/2026, 8:45:24 PM

--- 104-MaximumDepthofBinaryTree/104-MaximumDepthofBinaryTree.java
+++ 104-MaximumDepthofBinaryTree/104-MaximumDepthofBinaryTree.java
@@ -1,19 +1,26 @@
-// Last updated: 9/24/2026, 8:42:54 PM
-1class KthLargest {
-2    private int k;
-3    private PriorityQueue<Integer> pq;
-4    public KthLargest(int k, int[] nums) {
-5        this.k = k;
-6        pq = new PriorityQueue<>(k);
-7        for (int num : nums) {
-8            pq.offer(num);
-9            if (pq.size() > k) pq.poll(); 
-10        }
-11    }
-12    
-13    public int add(int val) {
-14        pq.offer(val); 
-15        if (pq.size() > k) pq.poll();
-16        return pq.peek();
-17    }
-18}
+// Last updated: 9/24/2026, 8:45:24 PM
+1class Solution {
+2        /*
+3        #########################################################################
+4        #                                                                       #
+5        #  =============================================                        #
+6        #                  SIDDARDHA CHILUVERU                                  #
+7        #  =============================================                        #
+8        #                                                                       #
+9        #  Author      : Siddardha Chiluveru                                    #
+10        #  Description : Solution / Code / Project                              #
+11        #  Date        : 2026-20-08                                             #
+12        #                                                                       #
+13        #########################################################################
+14        */
+15    public int findKthLargest(int[] nums, int k) {
+16    //     PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+17    //     for (int i : nums)
+18    //         pq.offer(i);
+19    //     while (k-- > 1)
+20    //         pq.poll();
+21    //     return pq.poll();
+22            Arrays.sort(nums);
+23            return nums[nums.length - k];
+24    }
+25}
