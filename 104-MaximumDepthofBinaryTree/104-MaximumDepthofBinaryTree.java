a7aff013fa4a93d5159b8af78a86c2c2ea236f41
Keerthiya S <keerthiyakeerthiya360@gmail.com>
2026-09-24 20:48:53 +0530

    File updated at 9/24/2026, 8:48:53 PM

--- 104-MaximumDepthofBinaryTree/104-MaximumDepthofBinaryTree.java
+++ 104-MaximumDepthofBinaryTree/104-MaximumDepthofBinaryTree.java
@@ -1,26 +1,36 @@
-// Last updated: 9/24/2026, 8:45:24 PM
+// Last updated: 9/24/2026, 8:48:53 PM
 1class Solution {
-2        /*
-3        #########################################################################
-4        #                                                                       #
-5        #  =============================================                        #
-6        #                  SIDDARDHA CHILUVERU                                  #
-7        #  =============================================                        #
-8        #                                                                       #
-9        #  Author      : Siddardha Chiluveru                                    #
-10        #  Description : Solution / Code / Project                              #
-11        #  Date        : 2026-20-08                                             #
-12        #                                                                       #
-13        #########################################################################
-14        */
-15    public int findKthLargest(int[] nums, int k) {
-16    //     PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
-17    //     for (int i : nums)
-18    //         pq.offer(i);
-19    //     while (k-- > 1)
-20    //         pq.poll();
-21    //     return pq.poll();
-22            Arrays.sort(nums);
-23            return nums[nums.length - k];
-24    }
-25}
+2    public void flatten(TreeNode root) {
+3        TreeNode curr = root;
+4
+5        while (curr != null) {
+6
+7            // If there is no left subtree,
+8            // the node is already in the correct position.
+9            if (curr.left == null) {
+10                curr = curr.right;
+11                continue;
+12            }
+13
+14            // Find the rightmost node of the left subtree
+15            TreeNode predecessor = curr.left;
+16
+17            while (predecessor.right != null) {
+18                predecessor = predecessor.right;
+19            }
+20
+21            // Connect the rightmost node of the left subtree
+22            // to the original right subtree.
+23            predecessor.right = curr.right;
+24
+25            // Move the left subtree to the right
+26            curr.right = curr.left;
+27
+28            // Left pointer must always be null
+29            curr.left = null;
+30
+31            // Move to the next node
+32            curr = curr.right;
+33        }
+34    }
+35}
