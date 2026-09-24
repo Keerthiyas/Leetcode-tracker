d773bb1b3ff3b8ae025d1dbd8d7ea3c75ad09b5a
Keerthiya S <keerthiyakeerthiya360@gmail.com>
2026-09-24 20:42:55 +0530

    File updated at 9/24/2026, 8:42:54 PM

--- 104-MaximumDepthofBinaryTree/104-MaximumDepthofBinaryTree.java
+++ 104-MaximumDepthofBinaryTree/104-MaximumDepthofBinaryTree.java
@@ -1,36 +1,19 @@
-// Last updated: 9/24/2026, 8:40:19 PM
-1class Solution {
-2    public TreeNode buildTree(int[] inorder, int[] postorder) {
-3        // Call the recursive function with full arrays and return the result
-4        return buildTree(inorder, 0, inorder.length - 1, postorder, 0, postorder.length - 1);
-5    }
-6    
-7    private TreeNode buildTree(int[] inorder, int inStart, int inEnd, int[] postorder, int postStart, int postEnd) {
-8        // Base case
-9        if (inStart > inEnd || postStart > postEnd) {
-10            return null;
-11        }
-12        
-13        // Find the root node from the last element of postorder traversal
-14        int rootVal = postorder[postEnd];
-15        TreeNode root = new TreeNode(rootVal);
-16        
-17        // Find the index of the root node in inorder traversal
-18        int rootIndex = 0;
-19        for (int i = inStart; i <= inEnd; i++) {
-20            if (inorder[i] == rootVal) {
-21                rootIndex = i;
-22                break;
-23            }
-24        }
-25        
-26        // Recursively build the left and right subtrees
-27        int leftSize = rootIndex - inStart;
-28        int rightSize = inEnd - rootIndex;
-29        root.left = buildTree(inorder, inStart, rootIndex - 1, postorder, postStart, postStart + leftSize - 1);
-30        root.right = buildTree(inorder, rootIndex + 1, inEnd, postorder, postEnd - rightSize, postEnd - 1);
-31        
-32        return root;
-33    }
-34}
-35
+// Last updated: 9/24/2026, 8:42:54 PM
+1class KthLargest {
+2    private int k;
+3    private PriorityQueue<Integer> pq;
+4    public KthLargest(int k, int[] nums) {
+5        this.k = k;
+6        pq = new PriorityQueue<>(k);
+7        for (int num : nums) {
+8            pq.offer(num);
+9            if (pq.size() > k) pq.poll(); 
+10        }
+11    }
+12    
+13    public int add(int val) {
+14        pq.offer(val); 
+15        if (pq.size() > k) pq.poll();
+16        return pq.peek();
+17    }
+18}
