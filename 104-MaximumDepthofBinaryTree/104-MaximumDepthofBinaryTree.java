0e696c3f48b83257e240d2a8426a5379c87286c8
Keerthiya S <keerthiyakeerthiya360@gmail.com>
2026-09-25 10:11:10 +0530

    File updated at 9/25/2026, 10:11:10 AM

--- 104-MaximumDepthofBinaryTree/104-MaximumDepthofBinaryTree.java
+++ 104-MaximumDepthofBinaryTree/104-MaximumDepthofBinaryTree.java
@@ -1,32 +1,35 @@
-// Last updated: 9/25/2026, 8:56:35 AM
+// Last updated: 9/25/2026, 10:11:10 AM
 1class Solution {
-2    public void solve(int[] nums, int index, List<Integer> output, Set<List<Integer>> ans) {
-3        // base case: if we have reached the end of the input array
-4        if (index >= nums.length) {
-5            // only add the output if it has more than one element
-6            if (output.size() > 1) {
-7                ans.add(new ArrayList<>(output));
-8            }
-9            return;
-10        }
-11        
-12        // if the output is empty or the current element is greater than or equal to the last element in the output
-13        if (output.isEmpty() || nums[index] >= output.get(output.size() - 1)) {
-14            // add the current element to the output and recursively call solve
-15            output.add(nums[index]);
-16            solve(nums, index+1, output, ans);
-17            // remove the last element from the output before returning
-18            output.remove(output.size() - 1);
-19        }
-20        
-21        // recursively call solve without adding the current element to the output
-22        solve(nums, index+1, output, ans);
-23    }
-24    
-25    public List<List<Integer>> findSubsequences(int[] nums) {
-26        // use a set to store the unique sub sequences
-27        Set<List<Integer>> ans = new HashSet<>();
-28        solve(nums, 0, new ArrayList<>(), ans);
-29        return new ArrayList<>(ans);
-30    }
-31}
+2    public List<List<Integer>> permute(int[] nums) {
+3        List<List<Integer>> res = new ArrayList<>();
+4        backtrack(nums, 0, res);
+5        return res;    
+6    }
+7
+8    private void backtrack(int[] nums, int start, List<List<Integer>> res) {
+9        if (start == nums.length) {
+10            res.add(arrayToList(nums));
+11            return;
+12        }
+13
+14        for (int i = start; i < nums.length; i++) {
+15            swap(nums, start, i);
+16            backtrack(nums, start + 1, res);
+17            swap(nums, start, i);
+18        }
+19    }
+20    
+21    private List<Integer> arrayToList(int[] arr) {
+22        List<Integer> list = new ArrayList<>();
+23        for (int num : arr) {
+24            list.add(num);
+25        }
+26        return list;
+27    }
+28    
+29    private void swap(int[] nums, int i, int j) {
+30        int temp = nums[i];
+31        nums[i] = nums[j];
+32        nums[j] = temp;
+33    }    
+34}
